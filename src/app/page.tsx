915dde43cf2b4872d452f0536cbcad3d10bedbc5
Jose Andres Macas Velez <jose.a.macas.v@unl.edu.ec>
2025-02-15 16:26:38 -0500

    Ventan principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
-'use client'
+'use client';
 
-import { Calendar } from 'primereact/calendar'
-import { useState } from 'react'
-import { Nullable } from 'primereact/ts-helpers'
+import { Calendar } from 'primereact/calendar';
+import { useState } from 'react';
+import { Nullable } from 'primereact/ts-helpers';
+import styles from './page.module.css';
+import Table from '@/components/table';
+import { Button } from 'primereact/button';
 
+export default function Home() {
+  const [date, setDate] = useState<Nullable<Date>>(null);
 
-export default function Home () {
-  const [date, setDate] = useState<Nullable<Date>>(null)
   return (
-    <main className='w-full h-screen grid'>
-      <section className='col-9'>
-        <h2>Lista de Ordenes</h2>
+    <main className={styles.container}>
+      <section className={styles.section}>
+        <h2>Lista de Órdenes</h2>
+        <Table />
+        <Button label="Nueva orden de trabajo" icon="pi pi-plus" className='mt-2' />
       </section>
-      <aside className='col-3'>
-        <Calendar value={date} onChange={(e) => setDate(e.value)} inline className="h-27rem w-18rem" />
+      <aside className={styles.aside}>
+        <h2>Calendario de entregas</h2>
+        <Calendar
+          value={date}
+          onChange={(e) => setDate(e.value)}
+          inline
+          className={styles.calendar}
+        />
       </aside>
-
     </main>
-  )
+  );
 }
