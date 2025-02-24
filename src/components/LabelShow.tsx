1d54fe96f79c7f8c0e0b0afe2545cfc53c3040e9
Jeremy <encaladajeremy@gmail.com>
2025-02-24 01:21:40 -0500

    Update LabelShow component to conditionally center value text based on layout order

--- src/components/LabelShow.tsx
+++ src/components/LabelShow.tsx
@@ -7,7 +7,7 @@ export const LabelShow: React.FC<LabelShowProps> = ({ label, value, order }) => 
   return(
     <div className={'label-show items-center gap-2 ' + (order === 'column' ? 'flex-col text-start' : 'flex-row')} >
       <span className={'label text-gray-400 text-md font-bold ' + (order === 'column' && 'w-fit')}>{label}:</span>
-      <span className='value'>{value}</span>
+      <span className={'value ' + (order === 'column' && 'text-center')} >{value}</span>
     </div>
   )
 }
