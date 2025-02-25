6d8b9dfa4d962d3b797ba02da16bb6555faa9a62
Jeremy <encaladajeremy@gmail.com>
2025-02-25 01:58:42 -0500

    Add new pages for Calendary, Cars, Clients, and Settings; enhance Home component loading logic

---
+++ src/app/calendary/page.tsx
@@ -0,0 +1,7 @@
+export default function Calendary () {
+  return (
+    <div>
+      <h1>Calendary</h1>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,25 @@ import { DataView } from 'primereact/dataview'
 import { OrdenTrabajo } from './types'
 import { ListOrders } from '@/components/listOrders'
 import { Loader } from '@/components/Loader/Loader'
+import { useEffect, useState } from 'react'
 
 export default function Home () {
-  const { orders, isLoading } = useOrders()
+  const [isLoading, setIsLoading] = useState(true)
+  const { orders } = useOrders()
   console.log(orders)
+
+  useEffect(() => {
+    if (orders) {
+      setIsLoading(false)
+    }
+  }, [orders])
 
   return (
     <>
       {isLoading ? <Loader widthPercentaje={10} heightPercentaje={50} /> :
-        (orders &&
-          <DataView value={orders} listTemplate={(items: OrdenTrabajo[]) => <ListOrders items={items} />} className='w-full px-5 pb-5 gap-2 w-10'  />
-        )
+        (orders && orders.length > 0) ? (
+          <DataView value={orders} listTemplate={(items: OrdenTrabajo[]) => <ListOrders items={items} />} className='px-0 pb-5 gap-2 w-10'  />
+        ) : (<Loader widthPercentaje={10} heightPercentaje={50} />)
       }
     </>
   )
