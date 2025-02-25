2e3516f3561febbf300d5189d2e63709c6478192
Jeremy <encaladajeremy@gmail.com>
2025-02-25 00:30:32 -0500

    Refactor ListOrders component to improve modal footer structure and enhance readability

--- src/components/listOrders.tsx
+++ src/components/listOrders.tsx
@@ -10,15 +10,16 @@ import { OrderView } from './OrderView'
 import { Button } from 'primereact/button'
 
 interface ListOrdersProps {
-    items: OrdenTrabajo[]
+  items: OrdenTrabajo[]
 }
-export const ListOrders = ({items} : ListOrdersProps) => {
+export const ListOrders = ({ items }: ListOrdersProps) => {
 
   const toast = useRef<Toast>(null)
   const [visible, setVisible] = useState(false)
   const [orderToShowInModal, setOrderToShowInModal] = useState<OrdenTrabajo | null>(null)
   const accept = () => {
     toast.current?.show({ severity: 'success', summary: 'Acción completada', detail: 'Orden eliminada con exito', life: 1000 })
+    hideModal()
   }
 
   const confirmDelete = () => {
@@ -32,13 +33,13 @@ export const ListOrders = ({items} : ListOrdersProps) => {
   }
 
   const showModal = (order: OrdenTrabajo) => {
-    if(visible) return
+    if (visible) return
     setOrderToShowInModal(order)
     setVisible(true)
   }
 
   const hideModal = () => {
-    if(!visible) return
+    if (!visible) return
     setVisible(false)
   }
 
@@ -49,16 +50,22 @@ export const ListOrders = ({items} : ListOrdersProps) => {
     return <ListItemOrder key={order.id} order={order} confirmDelete={confirmDelete} showModal={showModal} />
   })
 
+  const FooterModal = () => {
+    return(
+      <section className='flex gap-1.5 drop-shadow-lg pt-3'>
+        <Button icon='pi pi-pencil' severity='warning' className={'w-2 md:w-1 ' + ((orderToShowInModal?.estado !== EstadosOrden.FINALIZADA) ? '' : 'hidden  ')} />
+        <Button icon='pi pi-print' severity='help' className='w-2 md:w-1' />
+        <Button icon='pi pi-trash' severity='danger' className='w-2 ml-auto md:w-1' onClick={confirmDelete}/>
+      </section>
+    )
+  }
+
   return <>
-    <Toast ref={toast} position='bottom-right'/>
+    <Toast ref={toast} position='bottom-right' />
     <ConfirmDialog />
     {list}
-    <Dialog visible={visible} maximizable style={{ width: '95vw' }} onHide={hideModal} header={<h1 className='text-4xl text-primary font-bold'>Orden de trabajo N°{orderToShowInModal?.id}</h1>} contentClassName='px-0' footer={<section className='flex gap-1.5 drop-shadow-lg pt-3'>
-      <Button  icon='pi pi-pencil' severity='warning' className={'w-2 md:w-1 ' + ((orderToShowInModal?.estado !== EstadosOrden.FINALIZADA) ? '' : 'hidden  ')}/>
-      <Button icon='pi pi-print' severity='help' className='w-2 md:w-1'/>
-      <Button icon='pi pi-trash' severity='danger' className='w-2 ml-auto md:w-1'/>
-    </section>} >
-      <OrderView order={orderToShowInModal}/>
+    <Dialog visible={visible} maximizable style={{ width: '95vw' }} onHide={hideModal} header={<h1 className='text-4xl text-primary font-bold'>Orden de trabajo N°{orderToShowInModal?.id}</h1>} contentClassName='px-0' footer={<FooterModal/> } >
+      <OrderView order={orderToShowInModal} />
     </Dialog>
   </>
 }
