b8ae58a44476d113ab28c678f3e48c69c929289b
Jeremy Encalada <encaladajeremy@gmail.com>
2025-03-21 17:36:30 -0500

    Update NewOrder.tsx

--- src/components/NewOrder.tsx
+++ src/components/NewOrder.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import { Dialog } from 'primereact/dialog'
 import { InputText } from 'primereact/inputtext'
 import { Calendar } from 'primereact/calendar'
