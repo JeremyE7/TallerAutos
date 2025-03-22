139d4bb645cc45a3deeb8e8567c4f13372cfb2ca
Jeremy <encaladajeremy@gmail.com>
2025-03-21 23:08:12 -0500

    Enhance order data handling by adding date conversion and enum typing in getOrders function

--- src/utils/orders.ts
+++ src/utils/orders.ts
@@ -1,4 +1,4 @@
-import { OrdenTrabajo, Response } from '@/app/types'
+import { EstadosOrden, MetodoPago, OrdenTrabajo, Response } from '@/app/types'
 
 export const calcTotal = (order: OrdenTrabajo) => {
   //TOTAL = (Total M/O + Total REP) × (IVA)
@@ -23,64 +23,72 @@ export const getOrders = () => {
       }
 
       console.info(data.message)
-      return data.data
+
+      // Convertir fechas y enums
+      return data.data?.map(order => ({
+        ...order,
+        fechaIngreso: new Date(order.fechaIngreso),
+        fechaSalida: new Date(order.fechaSalida),
+        forma_pago: order.forma_pago as MetodoPago,
+        estado: order.estado as EstadosOrden
+      }))
     }
 
     // Si no hay datos en el localStorage, se crea y se guarda el objeto por defecto
     console.info('No orders found in local storage, creating default data...')
-    const defaultData = {
-      'message': 'Datos cargados correctamente',
-      'code': 200,
-      'data': [
+    const defaultData: Response<OrdenTrabajo[]> = {
+      message: 'Datos cargados correctamente',
+      code: 200,
+      data: [
         {
-          'id': 1,
-          'fechaIngreso': '2025-03-20T00:00:00.000Z',
-          'fechaSalida': '2025-03-21T00:00:00.000Z',
-          'operaciones_solicitadas': 'Cambio de aceite, revisión de frenos',
-          'total_mo': 50,
-          'total_rep': 30,
-          'iva': 10,
-          'total': 90,
-          'comentarios': 'Todo en orden',
-          'vehiculo': {
-            'id': 1,
-            'marca': 'Toyota',
-            'modelo': 'Corolla',
-            'anio': 2020,
-            'placa': 'ABC123',
-            'cliente': {
-              'id': 1,
-              'nombre': 'Juan Pérez',
-              'cedula': '123456789',
-              'direccion': 'Calle Ficticia 123',
-              'email': 'juanperez@mail.com',
-              'telefono': '0987654321'
+          id: 1,
+          fechaIngreso: new Date('2025-03-20T00:00:00.000Z'),
+          fechaSalida: new Date('2025-03-21T00:00:00.000Z'),
+          operaciones_solicitadas: 'Cambio de aceite, revisión de frenos',
+          total_mo: 50,
+          total_rep: 30,
+          iva: 10,
+          total: 90,
+          comentarios: 'Todo en orden',
+          vehiculo: {
+            id: 1,
+            marca: 'Toyota',
+            modelo: 'Corolla',
+            anio: 2020,
+            placa: 'ABC123',
+            cliente: {
+              id: 1,
+              nombre: 'Juan Pérez',
+              cedula: '123456789',
+              direccion: 'Calle Ficticia 123',
+              email: 'juanperez@mail.com',
+              telefono: '0987654321'
             }
           },
-          'elementosIngreso': {
-            'id': 1,
-            'limpiaparabrisas': true,
-            'espejos': true,
-            'luces': true,
-            'placas': true,
-            'emblemas': false,
-            'radio': true,
-            'control_alarma': false,
-            'tapetes': true,
-            'aire_acondicionado': true,
-            'matricula': true,
-            'herramientas': false,
-            'tuerca_seguridad': true,
-            'gata': false,
-            'llave_ruedas': true,
-            'extintor': true,
-            'encendedor': true,
-            'antena': true,
-            'llanta_emergencia': false,
-            'combustible': 50
+          elementosIngreso: {
+            id: 1,
+            limpiaparabrisas: true,
+            espejos: true,
+            luces: true,
+            placas: true,
+            emblemas: false,
+            radio: true,
+            control_alarma: false,
+            tapetes: true,
+            aire_acondicionado: true,
+            matricula: true,
+            herramientas: false,
+            tuerca_seguridad: true,
+            gata: false,
+            llave_ruedas: true,
+            extintor: true,
+            encendedor: true,
+            antena: true,
+            llanta_emergencia: false,
+            combustible: 50
           },
-          'forma_pago': 'Efectivo',
-          'estado': 'Pendiente'
+          forma_pago: MetodoPago.EFECTIVO,
+          estado: EstadosOrden.PENDIENTE
         }
       ]
     }
@@ -94,6 +102,7 @@ export const getOrders = () => {
     return []
   }
 }
+
 
 export const editOrder = (id: number, order: OrdenTrabajo) => {
   try {
