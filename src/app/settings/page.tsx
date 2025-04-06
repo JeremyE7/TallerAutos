8b127dbde8ba6677fd87224a0b558084b43a62a3
Jeremy <encaladajeremy@gmail.com>
2025-04-06 04:29:59 -0500

    Add Toast notification for successful server settings save in Settings component

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,12 +2,14 @@
 import { LabelShow } from "@/components/LabelShow";
 import { useSettings } from "@/hooks/useSettings";
 import { Button } from "primereact/button";
-import { useEffect, useState } from "react";
+import { Toast } from "primereact/toast";
+import { useEffect, useRef, useState } from "react";
 
 export default function Settings() {
 
   const {setServerSettings, clientKey } = useSettings()
   const [serverKeyAux, setServerKey] = useState(clientKey)
+  const ToastRef = useRef<Toast>(null)
 
   useEffect(() => {
     setServerKey(clientKey)
@@ -15,6 +17,7 @@ export default function Settings() {
 
   const handleSave = () => {
     setServerSettings(serverKeyAux)
+    ToastRef.current?.show({ severity: 'success', summary: 'Guardado', detail: 'Configuración guardada correctamente', life: 3000 })
   }
 
   const handleChangeKey = (value: string) => {
@@ -26,6 +29,7 @@ export default function Settings() {
     <section className="flex flex-col gap-4 p-4 ">
       <LabelShow label="Clave de servidor" editable value={serverKeyAux} order="column" onChange={(value) => handleChangeKey(value)}/>
       <Button label="Guardar" icon="pi pi-save" className="p-button-success" onClick={handleSave}/>
+      <Toast ref={ToastRef} position="bottom-right" baseZIndex={1000} />
     </section>
   )
 }
