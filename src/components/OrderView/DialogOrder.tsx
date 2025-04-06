44fd1df2537b690f04216d4e220460e9c3edb789
Jeremy <encaladajeremy@gmail.com>
2025-04-06 18:53:02 -0500

    Refactor DialogOrder component to remove Toast notifications and use settingsStore for success/error handling

--- src/components/OrderView/DialogOrder.tsx
+++ src/components/OrderView/DialogOrder.tsx
@@ -1,6 +1,6 @@
 import { OrdenTrabajo } from "@/app/types"
 import { useOrders } from "@/hooks/useOrders"
-import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog"
+import { confirmDialog } from "primereact/confirmdialog"
 import { Dialog } from "primereact/dialog"
 import { useRef, useState } from "react"
 import { OrderView } from "./OrderView"
@@ -9,6 +9,7 @@ import { FooterModal } from "./FooterModal"
 import { Toast } from "primereact/toast"
 import { ProgressSpinner } from "primereact/progressspinner"
 import { Button } from "primereact/button"
+import { settingsStore } from "@/store/settingsStore"
 
 interface DialogOrderProps {
     editedOrder: OrdenTrabajo | null
@@ -25,7 +26,7 @@ export const DialogOrder: React.FC<DialogOrderProps> = ({ editedOrder, setEdited
     const [edit, setEdit] = useState(false)
     const [isGeneratingPDF, setIsGeneratingPDF] = useState(false);
     const [pdfGenerationStatus, setPdfGenerationStatus] = useState('');
-    const toast = useRef<Toast>(null)
+    const {setSuccess, setError} = settingsStore()
 
     const printOrderPDF = async (order: OrdenTrabajo) => {
         if (!order) return;
@@ -57,10 +58,10 @@ export const DialogOrder: React.FC<DialogOrderProps> = ({ editedOrder, setEdited
         const succesEditedOrder = await saveEditedOrder(editedOrder)
         setIsLoading(false)
         if (succesEditedOrder) {
-            toast.current?.show({ severity: 'success', summary: 'Acción completada', detail: 'Orden editada con exito', life: 1000 })
+            setSuccess('Orden editada correctamente')
             setEdit(false)
         } else {
-            toast.current?.show({ severity: 'error', summary: 'Error', detail: 'Error al editar la orden', life: 1000 })
+            setError('Error al editar la orden')
         }
     }
 
@@ -75,10 +76,10 @@ export const DialogOrder: React.FC<DialogOrderProps> = ({ editedOrder, setEdited
         const success = await eliminateOrder(orderToShowInModal.id)
         setIsLoading(false)
         if (success) {
-            toast.current?.show({ severity: 'success', summary: 'Acción completada', detail: 'Orden eliminada con exito', life: 2000 })
+            setSuccess('Orden eliminada con éxito')
             onHide()
         } else {
-            toast.current?.show({ severity: 'error', summary: 'Error', detail: 'Error al eliminar la orden', life: 2000 })
+            setError('Error al eliminar la orden')
         }
     }
 
@@ -100,8 +101,6 @@ export const DialogOrder: React.FC<DialogOrderProps> = ({ editedOrder, setEdited
 
     return (
         <>
-            <Toast ref={toast} position='bottom-right' className='w-10 md:w-auto' />
-            <ConfirmDialog />
             <Dialog visible={visible} maximizable style={{ width: '95vw' }} onHide={handleHideModal} header={<HeaderOrderModal edit={edit} editedOrder={editedOrder} />} contentClassName='px-0' footer={<FooterModal confirmDelete={confirmDelete} edit={edit} handleCancelEdit={handleCancelEdit} handleSaveEdit={handleSaveEdit} isLoading={isLoading} orderToShowInModal={orderToShowInModal} printOrderPDF={printOrderPDF} setEdit={setEdit} />} >
                 <OrderView order={orderToShowInModal} edit={edit} editedOrder={editedOrder} setEditedOrder={setEditedOrder} setEdit={setEdit} />
             </Dialog>
